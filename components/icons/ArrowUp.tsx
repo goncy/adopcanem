27a3aa0c793ffe8b9608440debe7733e2cea8b44
MatiasMonasterio <matias_monasterio@outlook.com>
2022-05-18 15:06:44 -0300

    add icons component folder and ArrowUp icon
    Signed-off-by: MatiasMonasterio <matias_monasterio@outlook.com>

---
+++ components/icons/ArrowUp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const ArrowUp: React.FC = ({}) => {
+  return (
+    <svg fill="none" height="16" viewBox="0 0 25 16" width="25" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M24.15 12.9999L22 15.1499L12.1 5.2499L2.20005 15.1499L0.0500487 12.9999L12.1 0.949903L24.15 12.9999Z"
+        fill="#FAFAFC"
+      />
+    </svg>
+  );
+};
