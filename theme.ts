182b5d2bb3c579da4ccc7a12c0d7e902bf6c734f
goncy <gonzalo.pozzo4@gmail.com>
2022-05-18 15:37:24 -0300

    Fix build

--- theme.ts
+++ theme.ts
@@ -3,10 +3,8 @@ import {BadgeProps, extendTheme, theme} from "@chakra-ui/react";
 export default extendTheme({
   styles: {
     global: {
-      "html, body, #__next": {
-        height: "100%",
-      },
       body: {
+        minHeight: "100vh",
         backgroundColor: "gray.100",
         color: "black",
       },
