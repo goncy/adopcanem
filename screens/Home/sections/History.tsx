19d152612a7da6ae850f8509d2f28a08f16c9e19
Faustino Zanetto <faustizanetto@gmail.com>
2022-05-18 14:49:56 -0300

    Started improving History Section

--- screens/Home/sections/History.tsx
+++ screens/Home/sections/History.tsx
@@ -1,65 +1,111 @@
 import React from "react";
-import {Stack, Flex, Button, Badge, Text} from "@chakra-ui/react";
+import {Stack, Flex, Button, Badge, Text, VStack, Box, HStack} from "@chakra-ui/react";
+import Section from "../../../components/section/Section";
 
 const History: React.FC = () => {
   return (
-    <Stack
-      align="center"
-      backgroundColor="#E5E5E5"
-      direction="column"
-      justify={"center"}
-      paddingX={24}
-      paddingY={24}
-    >
-      <Badge
-        backgroundColor={"gray.300"}
-        colorScheme="gray"
-        fontSize="md"
-        fontWeight={600}
-        textColor="#363636"
-      >
-        Sumá con tu ayuda
-      </Badge>
-      <Text fontSize="4xl" fontWeight={800} textColor="#363636">
-        Nuestra historia
-      </Text>
-      <Text fontSize="md" maxWidth="640">
-        Existen muchas formas de ser parte de este cambio, desde algún lugar podes hacerlo. Los
-        aportes económicos nos ayudan a pagar controles, estudios, veterinarios, traslados y comprar
-        alimento.
-      </Text>
-      <Text fontSize="7xl" fontWeight={800} mt="94" textColor="#363636">
-        +3500
-      </Text>
-      <Flex justify="space-between" mt="25" width="full">
-        <Text fontSize="sm" fontWeight={800} textColor="#363636">
-          Comienza la organización 2017
-        </Text>
-        <Text fontSize="l" fontWeight={800} textColor="#363636">
-          Mascotas rescatadas
-        </Text>
-        <Text fontSize="s" fontWeight={800} textColor="#363636">
-          No más mascotas sin hogar 2022
-        </Text>
-      </Flex>
+    <Section backgroundColor="#E5E5E5" hasDivider={false}>
+      {/* Main stack */}
+      <VStack py={4} spacing={10} width="full">
+        {/* Top */}
+        <VStack spacing={2} textAlign="center" width="full">
+          <Badge
+            backgroundColor={"gray.300"}
+            colorScheme="gray"
+            fontSize="md"
+            fontWeight={800}
+            textColor="#363636"
+          >
+            Sumá con tu ayuda
+          </Badge>
+          <Text fontSize="5xl" fontWeight={900} textColor="#363636">
+            Nuestra historia
+          </Text>
+          <Text fontSize="lg" maxWidth="750" fontWeight={600}>
+            Existen muchas formas de ser parte de este cambio, desde algún lugar
+            podes hacerlo. Los aportes económicos nos ayudan a pagar controles,
+            estudios, veterinarios, traslados y comprar alimento.
+          </Text>
+        </VStack>
 
-      <Stack direction={"row"} paddingTop={70}>
-        <Button
-          _hover={{bg: "primary.500"}}
-          bg={"red.400"}
-          color={"white"}
-          paddingX="10"
-          paddingY="4"
-          size="xs"
-        >
-          ir a mercadopago
-        </Button>
-      </Stack>
+        {/* Timeline */}
+        <VStack spacing={2}>
+          <Text fontSize="7xl" fontWeight={800} textColor="#363636">
+            +3500
+          </Text>
+          <Flex justify="space-between" alignItems="flex-start" width="full">
+            <Text
+              fontSize="md"
+              fontWeight={800}
+              textColor="#363636"
+              textAlign="center"
+            >
+              Comienza la organización <br />
+              2017
+            </Text>
+            <Text fontSize="xl" fontWeight={800} textColor="#363636">
+              Mascotas rescatadas
+            </Text>
+            <Text
+              fontSize="md"
+              fontWeight={800}
+              textColor="#363636"
+              textAlign="center"
+            >
+              No más mascotas sin hogar <br />
+              2022
+            </Text>
+          </Flex>
+          <Box
+            width="1000px"
+            height="88px"
+            backgroundColor="rgba(253, 177, 69, 0.4)"
+            borderRadius="xl"
+            padding="15px"
+          >
+            <HStack
+              spacing={0}
+              alignItems="flex-start"
+              justifyContent="flex-start"
+            >
+              <Box
+                width="500px"
+                height="60px"
+                backgroundColor="white"
+                borderLeftRadius="xl"
+              />
+              <Box
+                _before={{
+                  content: "''",
+                  display: "block",
+                  position: "relative",
+                  width: "8px",
+                  left: "-5px",
+                  height: "60px",
+                  background: "primary.500",
+                }}
+              />
+            </HStack>
+          </Box>
+        </VStack>
 
-      <Text fontSize="xs" fontWeight={800} textColor="primary.500">
-        Prefiero hacer una transferencia
-      </Text>
-    </Stack>
+        <Stack direction={"row"} paddingTop={70}>
+          <Button
+            _hover={{ bg: "primary.500" }}
+            bg={"red.400"}
+            color={"white"}
+            paddingX={12}
+            size="lg"
+          >
+            Ir a mercadopago
+          </Button>
+        </Stack>
+
+        <Text fontSize="xs" fontWeight={800} textColor="primary.500">
+          Prefiero hacer una transferencia
+        </Text>
+      </VStack>
+    </Section>
   );
 };
 
