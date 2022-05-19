d76b60af9d0c3c14700aaa050c89f4e67a81cbe6
New Castile <carlosaguanipa1@gmail.com>
2022-05-19 15:59:49 -0400

    Formated code

--- screens/Home/sections/Family.tsx
+++ screens/Home/sections/Family.tsx
@@ -2,27 +2,27 @@ import {Box, Grid, Text, GridItem, Container, useBreakpointValue} from "@chakra-
 import React from "react";
 
 const FamilySection: React.FC = () => {
-  const display = useBreakpointValue({base: 'none', md: 'block' })
-  const colspan = useBreakpointValue({ base: 1, md: 2 })
+  const display = useBreakpointValue({base: "none", md: "block"});
+  const colspan = useBreakpointValue({base: 1, md: 2});
 
   return (
     <Container maxWidth="container.xl">
       <Grid
         backgroundColor="white"
         gap={3}
+        overflow="hidden"
+        position="relative"
         py={[5, 10]}
-        templateColumns={{ base:'0 repeat(4, 1fr) 0', md:'repeat(6, 1fr)' }}
+        templateColumns={{base: "0 repeat(4, 1fr) 0", md: "repeat(6, 1fr)"}}
         templateRows="repeat(3, 1fr)"
-        position='relative'
-        overflow='hidden'
       >
         <GridItem
           backgroundImage="url(https://www.etapainfantil.com/wp-content/uploads/2017/08/perros-familia-700x467.jpg)"
           backgroundPosition="center center"
           backgroundSize="cover"
           borderRadius="2xl"
-          minHeight={ 40}
-          display={ display }
+          display={display}
+          minHeight={40}
         />
         <GridItem
           backgroundImage="url(https://www.grupoyaakun.com/wp-content/uploads/2019/01/perro-familia.jpg)"
@@ -37,14 +37,20 @@ const FamilySection: React.FC = () => {
           backgroundSize="cover"
           borderRadius="2xl"
           colSpan={2}
-          rowSpan={3}
           colStart={3}
+          rowSpan={3}
         >
-          <Box 
-          bgGradient={'linear-gradient(0deg, gray.900 0%, transparent 30%)'}
-          borderRadius='2xl'
-          alignItems="flex-end" display="flex" h="100%" minHeight={80} justifyContent="center" padding={ [2, 5, 10]}>
-            <Text align="center" color="white" fontSize={['2xl',null,  '4xl']} fontWeight={700}>
+          <Box
+            alignItems="flex-end"
+            bgGradient={"linear-gradient(0deg, gray.900 0%, transparent 30%)"}
+            borderRadius="2xl"
+            display="flex"
+            h="100%"
+            justifyContent="center"
+            minHeight={80}
+            padding={[2, 5, 10]}
+          >
+            <Text align="center" color="white" fontSize={["2xl", null, "4xl"]} fontWeight={700}>
               Abraza la Familia
             </Text>
           </Box>
@@ -54,24 +60,23 @@ const FamilySection: React.FC = () => {
           backgroundPosition="center center"
           backgroundSize="cover"
           borderRadius="2xl"
-          rowSpan={2}
           colStart={5}
+          rowSpan={2}
         />
         <GridItem
           backgroundImage="url(https://img.freepik.com/foto-gratis/nino-jugando-sus-perros-al-aire-libre-familia_23-2148647817.jpg?size=626&ext=jpg)"
           backgroundPosition="center center"
           backgroundSize="cover"
           borderRadius="2xl"
-          display={ display }
-
+          display={display}
         />
         <GridItem
           backgroundImage="url(https://gestion.portalbiesa.com/redaccio/arxius/imatges/201810/770_1538733285blog_post_familia_1.jpg)"
           backgroundPosition="center center"
           backgroundSize="cover"
           borderRadius="2xl"
-          colSpan={ colspan }
           colEnd={3}
+          colSpan={colspan}
           // display={ display }
         />
         <GridItem
@@ -79,7 +84,7 @@ const FamilySection: React.FC = () => {
           backgroundPosition="center center"
           backgroundSize="cover"
           borderRadius="2xl"
-          display={ display }
+          display={display}
         />
         <GridItem
           backgroundImage="url(https://blog-cdn.dogbuddy.com/wp-content/uploads/2016/04/child-with-a-dog-e1461229430570.jpg)"
@@ -100,10 +105,9 @@ const FamilySection: React.FC = () => {
           backgroundPosition="center center"
           backgroundSize="cover"
           borderRadius="2xl"
-          colSpan={ colspan }
+          colSpan={colspan}
           colStart={5}
-        >
-        </GridItem>
+        />
       </Grid>
     </Container>
   );
