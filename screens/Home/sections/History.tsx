9736f06e90197414cf8110e77c111e7de6f56834
New Castile <carlosaguanipa1@gmail.com>
2022-05-19 15:48:18 -0400

    Changed timeline Flex component for Grid

--- screens/Home/sections/History.tsx
+++ screens/Home/sections/History.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Flex, Button, Badge, Text, VStack, Box, HStack} from "@chakra-ui/react";
+import {Button, Badge, Text, VStack, Box, HStack, Grid, GridItem} from "@chakra-ui/react";
 import Link from "next/link";
 
 import Section from "../../../components/section/Section";
@@ -31,46 +31,84 @@ const History: React.FC = () => {
         </VStack>
 
         {/* Timeline */}
-        <VStack spacing={2} width="full">
-          <Text fontSize="7xl" fontWeight={800} textColor="#363636">
-            +3500
-          </Text>
-          <Flex alignItems="flex-start" justify="space-between" width="full">
-            <Text fontSize="md" fontWeight={800} textAlign="center" textColor="#363636">
-              Comienza la organización <br />
-              2017
+        <Grid gap={2} templateColumns={"repeat(3, 1fr)"} width="full">
+          <GridItem>
+            <VStack h={"full"} justify={"center"}>
+              <Text
+                fontSize={{base: "sm", sm: "md"}}
+                fontWeight={800}
+                textAlign="center"
+                textColor="#363636"
+                width={"full"}
+              >
+                Comienza la organización <br />
+                2017
+              </Text>
+            </VStack>
+          </GridItem>
+          <GridItem>
+            <Text
+              fontSize={{base: "xl", sm: "3xl", md: "7xl"}}
+              fontWeight={800}
+              lineHeight={"normal"}
+              position={"relative"}
+              textAlign={"center"}
+              textColor="#363636"
+            >
+              +3500
             </Text>
-            <Text fontSize="xl" fontWeight={800} textColor="#363636">
-              Mascotas rescatadas
+            <Text
+              fontSize={{base: "md", sm: "lg", md: "xl"}}
+              fontWeight={800}
+              textAlign={"center"}
+              textColor="#363636"
+            >
+              Mascotas <br />
+              rescatadas
             </Text>
-            <Text fontSize="md" fontWeight={800} textAlign="center" textColor="#363636">
-              No más mascotas sin hogar <br />
-              2022
-            </Text>
-          </Flex>
-          <Box
-            backgroundColor="rgba(253, 177, 69, 0.4)"
-            borderRadius="xl"
-            height="88px"
-            padding="15px"
-            width="1000px"
-          >
-            <HStack alignItems="flex-start" justifyContent="flex-start" spacing={0}>
-              <Box backgroundColor="white" borderLeftRadius="xl" height="60px" width="500px" />
+          </GridItem>
+          <GridItem>
+            <VStack h={"full"} justify={"center"}>
+              <Text
+                fontSize={{base: "sm", sm: "md"}}
+                fontWeight={800}
+                textAlign="center"
+                textColor="#363636"
+              >
+                No más mascotas sin hogar
+                <br />
+                2022
+              </Text>
+            </VStack>
+          </GridItem>
+          <GridItem colSpan={3}>
+            <VStack>
               <Box
-                _before={{
-                  content: "''",
-                  display: "block",
-                  position: "relative",
-                  width: "8px",
-                  left: "-5px",
-                  height: "60px",
-                  background: "primary.500",
-                }}
-              />
-            </HStack>
-          </Box>
-        </VStack>
+                backgroundColor="rgba(253, 177, 69, 0.4)"
+                borderRadius="xl"
+                height="88px"
+                minWidth={""}
+                padding="15px"
+                width="80%"
+              >
+                <HStack alignItems="flex-start" justifyContent="flex-start" spacing={0}>
+                  <Box backgroundColor="white" borderLeftRadius="xl" height="60px" width="50%" />
+                  <Box
+                    _before={{
+                      content: "''",
+                      display: "block",
+                      position: "relative",
+                      width: "8px",
+                      left: "-5px",
+                      height: "60px",
+                      background: "primary.500",
+                    }}
+                  />
+                </HStack>
+              </Box>
+            </VStack>
+          </GridItem>
+        </Grid>
 
         {/* Bottom */}
         <VStack paddingTop={50} spacing={6} width="full">
