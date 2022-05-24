bad0f17369d879cd8e5f1e6cdbf6a6d3bc37d4f6
pablo-pc <pablo-362@hotmail.com>
2022-05-24 14:13:40 -0300

    feat: add about us page

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -7,43 +7,45 @@ import FooterDivider from "./FooterDivider";
 
 const Footer: React.FC = () => {
   return (
-    <Stack
-      alignItems="end"
-      backgroundColor="primary.400"
-      direction="row"
-      height="350px"
-      justifyContent="space-between"
-      mt={10}
-      padding="20px 50px"
-      paddingX={12}
-    >
+    <Stack spacing={0}>
       <FooterDivider />
-      <Text fontSize="lg" fontWeight={700} width="200px">
-        adopcanem - 2022
-      </Text>
       <Stack
-        alignSelf="start"
-        direction="column"
-        fontSize="lg"
-        fontWeight={700}
-        spacing={7}
-        textAlign="center"
+        alignItems="end"
+        backgroundColor="primary.400"
+        direction="row"
+        height="350px"
+        justifyContent="space-between"
+        mt={10}
+        padding="20px 50px"
+        paddingX={12}
       >
-        <FooterLink href="/">Encontrá tu mascota</FooterLink>
-        <FooterLink href="/">Sobre nosotros</FooterLink>
-        <FooterLink href="/">Contáctanos</FooterLink>
-        <FooterLink href="/">Hacé tu donación</FooterLink>
-      </Stack>
-      <Stack direction="row" gap="10px" justifyContent="end" width="200px">
-        <Link href="#">
-          <FiLinkedin size={24} />
-        </Link>
-        <Link isExternal href="https://www.facebook.com/Adopcanem">
-          <FiFacebook size={24} />
-        </Link>
-        <Link isExternal href="https://www.instagram.com/adopcanem/?hl=es-la">
-          <FiInstagram size={24} />
-        </Link>
+        <Text fontSize="lg" fontWeight={700} width="200px">
+          adopcanem - 2022
+        </Text>
+        <Stack
+          alignSelf="start"
+          direction="column"
+          fontSize="lg"
+          fontWeight={700}
+          spacing={7}
+          textAlign="center"
+        >
+          <FooterLink href="/">Encontrá tu mascota</FooterLink>
+          <FooterLink href="/">Sobre nosotros</FooterLink>
+          <FooterLink href="/">Contáctanos</FooterLink>
+          <FooterLink href="/">Hacé tu donación</FooterLink>
+        </Stack>
+        <Stack direction="row" gap="10px" justifyContent="end" width="200px">
+          <Link href="#">
+            <FiLinkedin size={24} />
+          </Link>
+          <Link isExternal href="https://www.facebook.com/Adopcanem">
+            <FiFacebook size={24} />
+          </Link>
+          <Link isExternal href="https://www.instagram.com/adopcanem/?hl=es-la">
+            <FiInstagram size={24} />
+          </Link>
+        </Stack>
       </Stack>
     </Stack>
   );
--- components/footer/FooterDivider.tsx
+++ components/footer/FooterDivider.tsx
@@ -7,25 +7,23 @@ const FooterDivider: React.FC<FooterDividerProps> = (props) => {
   const {} = props;
 
   return (
-    <Box bottom={350} left={0} overflow="hidden" position="absolute" width="100%">
-      <svg
+    <svg
+      fill="#F8F3E3"
+      preserveAspectRatio="none"
+      style={{
+        display: "block",
+        height: "100px",
+        position: "relative",
+        width: "100%",
+      }}
+      viewBox="0 0 1512 520"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 90.4716C585.649 -30.973 916.74 -29.3387 1512 90.4716V520H0V90.4716Z"
         fill="#F8F3E3"
-        preserveAspectRatio="none"
-        style={{
-          display: "block",
-          height: "100px",
-          position: "relative",
-          width: "100%",
-        }}
-        viewBox="0 0 1512 520"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M0 90.4716C585.649 -30.973 916.74 -29.3387 1512 90.4716V520H0V90.4716Z"
-          fill="#F8F3E3"
-        />
-      </svg>
-    </Box>
+      />
+    </svg>
   );
 };
 
